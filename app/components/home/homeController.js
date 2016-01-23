a4d085ab62824b5da9192f6004fb12b5788644d9
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-23 17:55:49 -0500

    finished clarifai service

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,4 +1,7 @@
-app.controller('homeController', ['$scope', '$fetchRecipe', function ($scope, $fetchRecipe) {
+app.controller('homeController', ['$scope', '$fetchRecipe', '$photoTags', function ($scope, $fetchRecipe, $photoTags) {
+  $photoTags.getTags(undefined, function (data) {
+    console.log(data);
+  });
   /*
   keywords = encodeURIComponent('chicken');
   $fetchRecipe.fetchJson(keywords, function (recipes) {
--- app/services/photoTags.js
+++ app/services/photoTags.js
@@ -1,5 +1,23 @@
 app.factory('$photoTags', ['$http', function ($http) {
 
+  var API_KEY = 'ofbtuaAoWchh2tVW63x2ax2XxBqXMj';
+  var AUTH = {
+    Authorization: 'Bearer' + ' ' + API_KEY
+  };
+
   return {
+    getTags: function (imgUrl, callback) {
+      imgUrl = typeof imgUrl === 'undefined' ? 'http://www.clarifai.com/img/metro-north.jpg' : imgUrl;
+      $http({
+        method: 'POST',
+        url: 'https://api.clarifai.com/v1/tag/', 
+        headers: AUTH,
+        data: {
+          url: imgUrl
+        }
+      }).then(function (data) {
+        callback(data);
+      });
+    }
   };
 }]);
