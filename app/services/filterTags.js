d2fdff76c7b09f3f26ec6cd59293af9c49b6ec3e
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-23 22:15:46 -0500

    finished tag filter service

--- app/services/filterTags.js
+++ app/services/filterTags.js
@@ -2,9 +2,12 @@ app.factory('filterTags', function () {
   return {
     filterArray: function (oldArray) {
       return oldArray.filter(function (file) {
+        var token;
         ingredients.forEach(function (item) {
-          return file === item;
+          if (file === item)
+            token = item;
         });
+        return file === token;
       });
     }
   };
