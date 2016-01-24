fe7a3d722f5e3b2c49f1463be04929f54c70e581
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-23 20:19:34 -0500

    integrate yummly service

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,12 +1,12 @@
 app.controller('homeController', ['$scope', '$fetchRecipe', '$photoTags', function ($scope, $fetchRecipe, $photoTags) {
   var tags;
+  var recipes;
   $photoTags.getTags(undefined, function (data) {
     tags = data.data.results[0].result.tag.classes;
   });
-  /*
   keywords = encodeURIComponent('chicken');
   $fetchRecipe.fetchJson(keywords, function (recipes) {
+    recipes = recipes.data.matches[0].recipeName;
     console.log(recipes);
   });
-  */
 }]);
--- app/services/fetchRecipe.js
+++ app/services/fetchRecipe.js
@@ -1,12 +1,14 @@
 app.factory('$fetchRecipe', ['$http', function ($http) {
 
-  var API_KEY = 'a23ed3b8d32f3bcb957f616adba1bf5b';
-  var RECIPE_URL = 'http://food2fork.com/api/search';
+  var API_ID = '_app_id=153b6086';
+  var API_KEY = '_app_key=f11781de9ab69b378898a8696158fe6e';
+  var RECIPE_URL = 'http://api.yummly.com/v1/api/recipes';
+  var JSONP_CB = '&callback=JSON_CALLBACK';
 
   return {
     // keyword is a encode URI string of keywords
     fetchJson: function (keywords, callback) {
-      $http.jsonp(RECIPE_URL + '?' + 'key=' + API_KEY + '&q=' + keywords + '&callback=JSON_CALLBACK').
+      $http.jsonp(RECIPE_URL + '?' + API_ID + '&' + API_KEY + '&q=' + keywords + JSONP_CB).
         then(function (recipes) {
           callback(recipes);
         });
