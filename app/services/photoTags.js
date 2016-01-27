b102b4d7a3d27c889798907e69ce0f89f682613f
jaxonL <lin.jiaxuan@hotmail.com>
2016-01-27 10:03:57 -0500

    comment on api key and accessors

--- app/services/photoTags.js
+++ app/services/photoTags.js
@@ -1,6 +1,8 @@
 app.factory('$photoTags', ['Upload', function (Upload) {
 
-  var API_KEY = 'T3vUNk9i851I2sC29doREGRfBkF3IJ';
+  var API_KEY = 'T3vUNk9i851I2sC29doREGRfBkF3IJ'; //new token generated jan 2 10AM
+  //client id: G917g09U3kiA-NH1CC-lrLXRO0UsXBd8kb6Q_-zF
+  //client secret: dEKnwt3eIET-anHpPElQrtUrV29HdLV4BjFwiv8H
   var AUTH = {
     Authorization: 'Bearer' + ' ' + API_KEY
   };
